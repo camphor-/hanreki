f8cca5a8a78d512749fdcb7eca1e046f4be85752
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-06-27 03:16:46 +0900

    Initial commit

---
+++ lib/hanreki.rb
@@ -0,0 +1,5 @@
+require "hanreki/version"
+
+module Hanreki
+  # Your code goes here...
+end
---
+++ spec/hanreki_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Hanreki do
+  it 'has a version number' do
+    expect(Hanreki::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
